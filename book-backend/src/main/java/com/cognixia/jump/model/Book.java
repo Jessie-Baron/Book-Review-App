4feae4cfc15a7b86688ad8fd348dfcd7424f5c81
Gavin Liburd <GavinLiburd901@gmail.com>
2023-09-12 10:49:44 -0400

    Add models

---
+++ book-backend/src/main/java/com/cognixia/jump/model/Book.java
@@ -0,0 +1,116 @@
+package com.cognixia.jump.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String author;
+
+    @Column
+    private String genre;
+
+
+    public Book() {
+    }
+
+    public Book(int id, String title, String author, String genre) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Book id(int id) {
+        setId(id);
+        return this;
+    }
+
+    public Book title(String title) {
+        setTitle(title);
+        return this;
+    }
+
+    public Book author(String author) {
+        setAuthor(author);
+        return this;
+    }
+
+    public Book genre(String genre) {
+        setGenre(genre);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Book)) {
+            return false;
+        }
+        Book book = (Book) o;
+        return id == book.id && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(genre, book.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, genre);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", title='" + getTitle() + "'" +
+            ", author='" + getAuthor() + "'" +
+            ", genre='" + getGenre() + "'" +
+            "}";
+    }
+
+}
---
+++ book-backend/src/main/java/com/cognixia/jump/model/UserBook.java
@@ -0,0 +1,115 @@
+package com.cognixia.jump.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Objects;
+
+public class UserBook {
+    
+    @Id
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private Integer userId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "book_id", referencedColumnName = "id")
+    private Integer bookId;
+
+    @Column
+    private Integer rating;
+
+
+    public UserBook() {
+    }
+
+    public UserBook(Integer id, Integer userId, Integer bookId, Integer rating) {
+        this.id = id;
+        this.userId = userId;
+        this.bookId = bookId;
+        this.rating = rating;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getBookId() {
+        return this.bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public Integer getRating() {
+        return this.rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public UserBook id(Integer id) {
+        setId(id);
+        return this;
+    }
+
+    public UserBook userId(Integer userId) {
+        setUserId(userId);
+        return this;
+    }
+
+    public UserBook bookId(Integer bookId) {
+        setBookId(bookId);
+        return this;
+    }
+
+    public UserBook rating(Integer rating) {
+        setRating(rating);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof UserBook)) {
+            return false;
+        }
+        UserBook userBook = (UserBook) o;
+        return Objects.equals(id, userBook.id) && Objects.equals(userId, userBook.userId) && Objects.equals(bookId, userBook.bookId) && Objects.equals(rating, userBook.rating);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, bookId, rating);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", userId='" + getUserId() + "'" +
+            ", bookId='" + getBookId() + "'" +
+            ", rating='" + getRating() + "'" +
+            "}";
+    }
+
+}
